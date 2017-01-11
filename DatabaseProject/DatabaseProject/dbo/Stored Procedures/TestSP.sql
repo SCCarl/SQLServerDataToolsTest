af5098d6dee8bfcf5f1f491f32a290d3d9710246
Carl Upton <carl@schoolcomms.co.uk>
2017-01-11 18:14:37 +0000

    Adding db

---
+++ DatabaseProject/DatabaseProject/dbo/Stored Procedures/TestSP.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[TestSP]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM dbo.TestTable WHERE Id = 0
+END
+s
---
+++ DatabaseProject/DatabaseProject/dbo/Tables/TestTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TestTable] (
+    [Id]      BIGINT       IDENTITY (1, 1) NOT NULL,
+    [Name]    VARCHAR (50) NULL,
+    [Address] VARCHAR (50) NULL,
+    [Age]     INT          NULL,
+    CONSTRAINT [PK_TestTable] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
